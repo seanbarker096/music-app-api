b238ab2644cd95e3a9945ecb7183fd3b81ad71bf
Sean Barker <sean.barker096@gmail.com>
2022-09-11 11:52:16 +0100

    Force TCP connection by using IP address instead of localhost

--- scripts/api-integration-test.sh
+++ scripts/api-integration-test.sh
@@ -6,11 +6,22 @@
 ### DB Container ###
 export MYSQL_ROOT_PASSWORD='mysqlroot'
 export MY_SQL_CONTAINER_NAME='integration-test-mysql-host'
-# build the container. TODO: Remove --build if dont want to rebuild every time
-docker compose -f ../test/integration/docker-compose.yml up integration-test-mysql --build -d 
+
+echo "Checking for old containers and volumes..."
+
+if [[ ! -z "`docker compose -f ../test/integration/docker-compose.yml ps -q`" ]]; then
+    echo "Cleaning up old containers and volumes..."
+    docker compose -f ../test/integration/docker-compose.yml down -v
+else 
+    echo "None found"
+fi 
 
 ## Wait for mysql container to be up before running tests
 echo "Wait for docker mysql container to be running..."
+
+
+# build the container. TODO: Remove --build if dont want to rebuild every time
+docker compose -f ../test/integration/docker-compose.yml up integration-test-mysql -d 
 
 until [[ "`docker inspect -f {{.State.Health.Status}} $MY_SQL_CONTAINER_NAME`"  == "healthy" ]];
 do
@@ -18,7 +29,7 @@ do
 done
 echo "container is up"
 
-## Need to active pyenv environment befor running this script. Also cd out into parent dir so that 
-## All our different python packages can access eachother
+# Need to active pyenv environment befor running this script. Also cd out into parent dir so that All our different python packages can access eachother
 cd ../
+
 python -m pytest
