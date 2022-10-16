6f2d8a834c23abec74a371114bfcf6543e156ab1
Sean Barker <sean.barker096@gmail.com>
2022-10-17 00:06:10 +0100

    debugging

--- api/rest/file_service_api.py
+++ api/rest/file_service_api.py
@@ -9,8 +9,10 @@ blueprint = flask.Blueprint("file_service", __name__)
 @blueprint.route("/upload/", methods=["POST"])
 def upload():
     """Upload the file meta data and return the file upload location."""
-    json_data = flask.request.json
 
+    json_data = flask.request.json
+    print("json_data", json_data)
+    # TODO: Validate the request
     request = FileUploadRequest(
         uuid=json_data["uuid"], mime_type=json_data["mime_type"], file_size=None
     )
@@ -31,16 +33,17 @@ def upload():
 
 
 @blueprint.route("/upload/<string:file_uuid>", methods=["PATCH"])
-def upload_file_bytes():
+def upload_file_bytes(file_uuid: str):
     """Upload the file and create the download url."""
-    ## uploads
+    ## TODO: BUild the request using file_uuid and bytes we receive
+
+    print("file uuid", file_uuid)
+    print(flask.request.data)
     # file_bytes = flask.data
 
     # request = FileUpdateRequest(bytes=file_bytes)
 
     # response = flask.current_app.conns.file_service.update_file()
-
-    print("running test route")
 
     return True
 
