fac4303833945e3fc3eed3f978377f288dec1779
Sean Barker <sean.barker096@gmail.com>
2022-11-08 01:00:04 +0000

    Start to add users table to db seed

--- test/integration/seed.sql
+++ test/integration/seed.sql
@@ -22,9 +22,9 @@ CREATE TABLE gigs.post_attachments (
 
 DROP TABLE IF EXISTS gigs.files;
 CREATE TABLE gigs.files (
-    id int(10) unsigned auto_increment,
+    id bigint(10) unsigned auto_increment,
     uuid varchar(255) NOT NULL,
-    file_name varchar(255) NOT NULL,
+    file_name varchar(1024) NOT NULL,
     file_size int(10) NULL,
     mime_type varchar(255) NOT NULL,
     download_url varchar(255) NULL,
@@ -42,3 +42,13 @@ CREATE TABLE gigs.auth_tokens (
   UNIQUE INDEX owner_id_idx(owner_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
+
+DROP TABLE IF EXISTS gigs.users;
+CREATE TABLE gigs.users (
+  id int(10) unsigned auto_increment,
+  username varchar(16) NOT NULL,
+  first_name varchar(255) NOT NULL,
+  second_name varchar(255) NOT NULL,
+  create_time datetime DEFAULT '0000-00-00 00:00:00',
+  is_deleted tinyint(1) NOT NULL DEFAULT '0'
+)
