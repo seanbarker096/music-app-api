5bc59a8e1cebdcb012939c81dac86fa8cf49a3a1
Sean Barker <sean.barker096@gmail.com>
2023-04-02 14:29:35 +0100

    remove commented out old DB class

--- api/db/db.py
+++ api/db/db.py
@@ -9,75 +9,6 @@ from mysql.connector.cursor import MySQLCursor
 from api.db.config import DBConfig
 from exceptions.db.exceptions import DBDuplicateKeyException
 
-## TODO: Make agnostic of the platform hosting our DB
-## TODO: Make this a singleton class as don't want multiple connections all over the place
-
-
-class DBResult(object):
-    _cursor: MySQLCursor = ...
-
-    def __init__(self, cursor: MySQLCursor):
-        self._cursor = cursor
-
-    def get_last_row_id(self):
-        return self._cursor.lastrowid
-
-    def get_rows(self) -> tuple[dict[str, Any], ...]:
-        return self._cursor.fetchall()
-
-    def affected_rows(self) -> int:
-        """Number of affected rows (or selected rows for SELECT statements)"""
-        return self._cursor.rowcount
-
-
-# class DB:
-#     def __init__(self, config: Dict[str, str]):
-#         self.connection = None
-
-#         db_config = config["config_file"]["db"]
-
-#         if (
-#             db_config
-#             and db_config["host"]
-#             and db_config["user"]
-#             and db_config["password"]
-#             and db_config["database"]
-#             and db_config["port"]
-#         ):
-#             self.config = DBConfig(
-#                 db_config["host"],
-#                 db_config["user"],
-#                 db_config["password"],
-#                 db_config["database"],
-#                 db_config["port"],
-#             )
-#         else:
-#             raise Exception("Failed to instantiate DB class. Invalid configuration supplied")
-
-#         self.connection = pymysql.connect(
-#             host=self.config.host,
-#             user=self.config.user,
-#             password=self.config.password,
-#             database=self.config.database,
-#             port=int(self.config.port),
-#             cursorclass=pymysql.cursors.DictCursor,
-#             autocommit=True,  # TODO: Can disable this for readonly transactions
-#         )
-
-#     def run_query(self, sql, binds) -> DBResult:
-#         ## TODO: validate sql and binds
-#         ## TODO: Check connection exists
-
-#         ## TODO: What if connection fails? Shoulld i return None, throw error??
-
-#         try:
-#             with self.connection.cursor() as cursor:
-#                 cursor.execute(sql, binds)
-#             return DBResult(cursor=cursor)
-#         except pymysql.err.IntegrityError as e:
-#             print(e)
-#             raise DBDuplicateKeyException(e.args[1])
-
 
 class DBConnection:
     _cursor: MySQLCursor | None = ...
@@ -146,13 +77,13 @@ class DBConnection:
     def _open(self):
         if not self.opened:
             self.connection = pymysql.connect(
-            host=self.app_config.host,
-            user=self.app_config.user,
-            password=self.app_config.password,
-            database=self.app_config.database,
-            port=int(self.app_config.port),
-            cursorclass=pymysql.cursors.DictCursor,
-            autocommit=False,  # TODO: Can disable this for readonly transactions
+                host=self.app_config.host,
+                user=self.app_config.user,
+                password=self.app_config.password,
+                database=self.app_config.database,
+                port=int(self.app_config.port),
+                cursorclass=pymysql.cursors.DictCursor,
+                autocommit=False,  # TODO: Can disable this for readonly transactions
             )
             self.opened = True
 
