563f9a52bf469f948eaba064c481a6a77dd9715f
Sean Barker <sean.barker096@gmail.com>
2023-06-12 22:05:57 +0100

    remove .env files as not used

--- api/server.py
+++ api/server.py
@@ -34,31 +34,31 @@ filename = os.path.join(flask.helpers.get_root_path(__name__), "config", f"{env}
 
 config.read(filename)
 
-app = FlaskApp(config)
-app.config["MAX_CONTENT_LENGTH"] = 50 * 1000 * 1000  # TODO: Come up with sensible max file size
+application = FlaskApp(config)
+application.config["MAX_CONTENT_LENGTH"] = 50 * 1000 * 1000  # TODO: Come up with sensible max file size
 origin = config.get("cors", "origin")
-CORS(app, origins=[origin])
+CORS(application, origins=[origin])
 
 
 
-app.register_blueprint(file_service_api.blueprint, url_prefix="/api/fileservice/0.1")
-app.register_blueprint(posts_api.blueprint, url_prefix="/api/posts/0.1")
-app.register_blueprint(auth_api.blueprint, url_prefix="/api/auth/0.1")
-app.register_blueprint(users_api.blueprint, url_prefix="/api/users/0.1")
-app.register_blueprint(performer_api.blueprint, url_prefix="/api/performers/0.1")
-app.register_blueprint(features_api.blueprint, url_prefix="/api/features/0.1")
-app.register_blueprint(tags_api.blueprint, url_prefix="/api/tags/0.1")
-app.register_blueprint(performances_api.blueprint, url_prefix="/api/performances/0.1")
-app.register_blueprint(events_api.blueprint, url_prefix="/api/events/0.1")
+application.register_blueprint(file_service_api.blueprint, url_prefix="/api/fileservice/0.1")
+application.register_blueprint(posts_api.blueprint, url_prefix="/api/posts/0.1")
+application.register_blueprint(auth_api.blueprint, url_prefix="/api/auth/0.1")
+application.register_blueprint(users_api.blueprint, url_prefix="/api/users/0.1")
+application.register_blueprint(performer_api.blueprint, url_prefix="/api/performers/0.1")
+application.register_blueprint(features_api.blueprint, url_prefix="/api/features/0.1")
+application.register_blueprint(tags_api.blueprint, url_prefix="/api/tags/0.1")
+application.register_blueprint(performances_api.blueprint, url_prefix="/api/performances/0.1")
+application.register_blueprint(events_api.blueprint, url_prefix="/api/events/0.1")
 
 
-@app.before_request
+@application.before_request
 def create_db_connection():
     request_id = str(uuid.uuid4())
     flask.request.request_id = request_id
     DBConnection.instance(DBConnection, instance_key=request_id, config={'config_file': config})
 
-@app.after_request
+@application.after_request
 def after_request(response):
     db_connection_uuid = flask.request.request_id
 
@@ -71,4 +71,4 @@ def after_request(response):
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
