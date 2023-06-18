d07540df949780816b6b7768d887096137f2da70
Sean Barker <sean.barker096@gmail.com>
2023-06-18 22:07:58 +0100

    update

--- scripts/seed_rds.sh
+++ scripts/seed_rds.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Retrieve RDS environment variables
-RDS_INFO=$(/opt/elasticbeanstalk/bin/get-config addons -a rds)
-HOST=$(echo "$RDS_INFO" | jq -r '.env.RDS_HOSTNAME')
-PORT=$(echo "$RDS_INFO" | jq -r '.env.RDS_PORT')
-DB_NAME=$(echo "$RDS_INFO" | jq -r '.env.RDS_DB_NAME')
-USERNAME=$(echo "$RDS_INFO" | jq -r '.env.RDS_USERNAME')
-PASSWORD=$(echo "$RDS_INFO" | jq -r '.env.RDS_PASSWORD')
+HOST="$RDS_HOSTNAME"
+PORT="$RDS_PORT"
+DB_NAME="$RDS_DB_NAME"
+USERNAME="$RDS_USERNAME"
+PASSWORD="$RDS_PASSWORD"
 
 echo "Seeding RDS database...\n"
 # Path to your SQL file
